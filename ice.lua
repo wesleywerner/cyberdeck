3358a7bb30ae2fa636cc9b91b9e987d1ebc0bcce
Wesley <wesley.werner@gmail.com>
2017-08-15 21:20:16 +0200

    Remove oop methods for the ICE module.

--- ice.lua
+++ ice.lua
@@ -69,90 +69,78 @@
 Ice = {}
 Ice.MAX_HEALTH = 20
 
--- Constructor
--- the __call metamethod allows us to call the table like a function,
--- this becomes a constructor for creating new instances.
-setmetatable( Ice, {
-  __call = function( cls, typeName, rating, flags)
+function Ice:create(class, rating, flags)
 
-    -- new instance
-    local instance = {}
-    
-    --[[
-      the __index metatable redirects function-calls on any instances to
-      this base table (ie inheritance), and
-      "cls" refers to the current table
-      ]]
-    setmetatable( instance, { __index=cls } )
-    
-    -- validate the given values
-    if not cls.types[typeName] then
-      error (string.format("%q is not a valid ICE type.", typeName))
-    end
-    
-    if not rating or rating < 1 then
-      error (string.format("%q is not a valid rating for ICE.", rating or "nil" ))
-    end
-    
-    -- assign the given values
-    instance.name = nil
-    instance.typeName = typeName
-    instance.rating = rating
-    instance.health = Ice.MAX_HEALTH
-    -- number of turns to slow the ICE - skips every other turn - Affected by slow program
-    instance.slowLevel = nil
-    -- is confused for this many turns - Affected by confusion program
-    instance.confusionLevel = nil
-    -- weakened for this many turns - Affected by weaken program
-    instance.weakenLevel = nil
-    -- take damage this many turns - Affected by virus program
-    instance.virusLevel = nil
-    -- the analyzed level this ICE was subjected against
-    instance.analyzedLevel = 0
-    -- was this ICE bypassed by the player
-    instance.bypassed = nil
-    -- was this ICE accessed by the player
-    instance.accessed = nil
-    -- home node of this ICE
-    instance.homeNode = nil
-    -- current location
-    instance.node = nil
-    -- position within the node
-    instance.nodeDrawPosition = nil
-    -- the node this ICE is moving towards
-    instance.targetNode = nil
-    -- last direction moved
-    instance.lastMoveDirection = nil
-    -- see the list of ICE state at the top of this file
-    instance.state = nil
-    --  try signal a red alert
-    instance.hostile = nil
-    -- actively wanders about searching for the player. also navigates to the player on alerts.
-    instance.response = nil
-
-    -- BEHAVIOUR FLAGS
-    -- Resistant to non-piercing attacks
-    instance.hardened = false
-    -- Resistant to non-area attacks
-    instance.phasing = false
-    -- Can crash your programs on succesful hits
-    instance.crasher = false
-    -- sends power surges through your cyberdeck directly into your brain to cause mental damage
-    instance.lethal = false
-    -- destroys a file when it dies
-    instance.databomb = false
-    -- dump the decker out of the matrix
-    instance.dumper = false
-    -- attempt to fry a random one of the player's chips on successful dump
-    instance.fryer = false
-    -- apply any given flags
-    instance:applyFlags(flags)
-    
-    instance.name = instance:getDefaultName()
-    return instance
+  -- new instance
+  local instance = {}
   
+  -- validate the given values
+  if not self.types[class] then
+    error (string.format("%q is not a valid ICE class.", class))
   end
-})
+  
+  if not rating or rating < 1 then
+    error (string.format("%q is not a valid rating for ICE.", rating or "nil" ))
+  end
+  
+  -- assign the given values
+  instance.name = nil
+  instance.class = class
+  instance.rating = rating
+  instance.health = Ice.MAX_HEALTH
+  -- number of turns to slow the ICE - skips every other turn - Affected by slow program
+  instance.slowLevel = nil
+  -- is confused for this many turns - Affected by confusion program
+  instance.confusionLevel = nil
+  -- weakened for this many turns - Affected by weaken program
+  instance.weakenLevel = nil
+  -- take damage this many turns - Affected by virus program
+  instance.virusLevel = nil
+  -- the analyzed level this ICE was subjected against
+  instance.analyzedLevel = 0
+  -- was this ICE bypassed by the player
+  instance.bypassed = nil
+  -- was this ICE accessed by the player
+  instance.accessed = nil
+  -- home node of this ICE
+  instance.homeNode = nil
+  -- current location
+  instance.node = nil
+  -- position within the node
+  instance.nodeDrawPosition = nil
+  -- the node this ICE is moving towards
+  instance.targetNode = nil
+  -- last direction moved
+  instance.lastMoveDirection = nil
+  -- see the list of ICE state at the top of this file
+  instance.state = nil
+  --  try signal a red alert
+  instance.hostile = nil
+  -- actively wanders about searching for the player. also navigates to the player on alerts.
+  instance.response = nil
+
+  -- BEHAVIOUR FLAGS
+  -- Resistant to non-piercing attacks
+  instance.hardened = false
+  -- Resistant to non-area attacks
+  instance.phasing = false
+  -- Can crash your programs on succesful hits
+  instance.crasher = false
+  -- sends power surges through your cyberdeck directly into your brain to cause mental damage
+  instance.lethal = false
+  -- destroys a file when it dies
+  instance.databomb = false
+  -- dump the decker out of the matrix
+  instance.dumper = false
+  -- attempt to fry a random one of the player's chips on successful dump
+  instance.fryer = false
+  -- apply any given flags
+  self:applyFlags(instance, flags)
+  
+  instance.name = self:getDefaultName(instance)
+  return instance
+
+end
 
 -- There are a couple types of ICE, some of them have optional flags
 -- to change their behaviour. Some of these flags changes the ICE's name.
@@ -424,7 +412,7 @@ Ice.alternateNames = {
 }
 
 -- apply the given flags, a table of string values, to the ICE
-function Ice:applyFlags(flags)
+function Ice:applyFlags(ice, flags)
   local allowedFlags = {
     ["hardened"] = true,
     ["phasing"] = true,
@@ -439,104 +427,99 @@ function Ice:applyFlags(flags)
       if not allowedFlags[v] then
         error(string.format("%q is not a valid ICE flag", v))
       else
-        self[v] = true
+        ice[v] = true
       end
     end
   end
 end
 
-function Ice:getType()
-  local def = self.types[self.typeName]
+function Ice:getType(ice)
+  local def = self.types[ice.class]
   if not def then
-    error("No type definition found for %q", self.typeName)
+    error("No type definition found for %q", ice.class)
   end
   return def
 end
 
-function Ice:getName()
-  return self.name
+function Ice:getName(ice)
+  return ice.name
 end
 
-function Ice:getDefaultName()
+function Ice:getDefaultName(ice)
   local nameList = nil
-  if self.hardened then
+  if ice.hardened then
     nameList = self.alternateNames["hardened"]
-  elseif self.phasing then
+  elseif ice.phasing then
     nameList = self.alternateNames["phasing"]
-  elseif self.crasher then
+  elseif ice.crasher then
     nameList = self.alternateNames["crasher"]
-  elseif self.lethal then
+  elseif ice.lethal then
     nameList = self.alternateNames["lethal"]
-  elseif self.databomb then
+  elseif ice.databomb then
     nameList = self.alternateNames["data bomb"]
-  elseif self.fryer then
+  elseif ice.fryer then
     nameList = self.alternateNames["fryer"]
-  elseif self.dumper then
+  elseif ice.dumper then
     nameList = self.alternateNames["dumper"]
   else
-    local def = self:getType()
+    local def = self:getType(ice)
     nameList = def.names
   end
   
   if not nameList then
-    error(string.format("%q has no namelist", self.typeName))
+    error(string.format("%q has no namelist", ice.class))
   end
   
   -- get the name related to rating, clamped to list size
-  if self.rating < #nameList then
-    return nameList[self.rating]
+  if ice.rating < #nameList then
+    return nameList[ice.rating]
   else
     return nameList[#nameList]
   end
 end
 
-function Ice:getPrice()
-  local def = self:getType()
-  return 42;
-end
-
-function Ice:getNotes()
-  local def = self:getType()
-  if self.analyzedLevel == 0 then
+function Ice:getNotes(ice)
+  local def = self:getType(ice)
+  if ice.analyzedLevel == 0 then
     -- give the ICE type note
     return def.note
   else
     -- add extra notes for the behaviour flags
     local extraNotes = {}
-    if self.hardened then
+    if ice.hardened then
       table.insert(extraNotes, "Resistant to non-piercing attacks.")
     end
-    if self.phasing then
+    if ice.phasing then
       table.insert(extraNotes, "Resistant to non-area attacks.")
     end
-    if self.crasher then
+    if ice.crasher then
       table.insert(extraNotes, "Attacks can crash your programs.")
     end
-    if self.lethal then
+    if ice.lethal then
       table.insert(extraNotes, "Attacks can cause you mental damage.")
     end
-    if self.dumper then
+    if ice.dumper then
       table.insert(extraNotes, "Can dump your deck from the matrix.")
     end
-    if self.fryer then
+    if ice.fryer then
       table.insert(extraNotes, "Can fry one of your hardware chips.")
     end
     return def.note .. " " .. table.concat(extraNotes, " ")
   end
 end
 
-function Ice:getText()
-  return string.format("%s (%s %d)", self:getName(), self.typeName, self.rating)
+function Ice:getText(ice)
+  return string.format("%s (%s %d)", self:getName(ice), ice.class, ice.rating)
 end
 
 -- get the ICE rating, adjusted by factors like health, 
 -- if the ICE was analyzed and any weakened effects.
 -- larger results indicate a favorable outcome for the ICE.
 -- give optional parameter "versusHardwareOrOtherICE" as true to ignore player analysis effects.
-function Ice:getRating(versusHardwareOrOtherICE)
+function Ice:getRating(ice, versusHardwareOrOtherICE)
 
   -- use the base rating
-	local nRating = self.rating
+	local nRating = ice.rating
 
 	-- adjust according to ICE health. The curve of this can be seen with
   -- this code:
@@ -544,16 +527,16 @@ function Ice:getRating(versusHardwareOrOtherICE)
   --    print(string.format("%d%% ICE health reduces rating by -%.2f",n/20*100,(20-n)/4))
   --  end
   -- this was called GetConditionModifier() in the original source.
-	nRating = nRating - ((Ice.MAX_HEALTH - self.health)/4)
+	nRating = nRating - ((Ice.MAX_HEALTH - ice.health)/4)
 
 	-- reduce if the ICE is weakened
-	if self.weakened then
+	if ice.weakened then
     nRating = nRating - 4
   end
 
 	-- reduce if the ICE was analyzed
 	if not versusHardwareOrOtherICE then
-    nRating = nRating - self.analyzedLevel
+    nRating = nRating - ice.analyzedLevel
   end
 
 	return nRating
@@ -561,9 +544,9 @@ end
 
 -- get the ICE rating adjusted for combat.
 -- non-combat ICE take a penalty.
-function Ice:getAttackRating(versusHardwareOrOtherICE)
-  local nRating = self:getRating(versusHardwareOrOtherICE)
-  local def = self:getType()
+function Ice:getAttackRating(ice, versusHardwareOrOtherICE)
+  local nRating = self:getRating(ice, versusHardwareOrOtherICE)
+  local def = self:getType(ice)
   if not def.isCombat then
     return nRating - 2
   else
@@ -574,9 +557,9 @@ end
 -- get the ICE rating adjusted for sensors.
 -- used when calculating odds of hide and deceive programs against ICE.
 -- non-combat ICE get a bonus if alarm set.
-function Ice:getSensorRating(versusHardwareOrOtherICE)
-  local nRating = self:getRating(versusHardwareOrOtherICE)
-  local def = self:getType()
+function Ice:getSensorRating(ice, versusHardwareOrOtherICE)
+  local nRating = self:getRating(ice, versusHardwareOrOtherICE)
+  local def = self:getType(ice)
   -- TODO check if current system alert is not green
   if not def.isCombat then
     return nRating + 2
@@ -585,7 +568,7 @@ function Ice:getSensorRating(versusHardwareOrOtherICE)
   end
 end
 
-function Ice:setState(newstate)
+function Ice:setState(ice, newstate)
   local allowedStates = {
     ["inactive"] = true,
     ["guarding"] = true,
@@ -602,7 +585,7 @@ function Ice:setState(newstate)
   if not allowedStates[newstate] then
     error(string.format("%q is not a valid ICE state"))
   end
-  self.state = newstate
+  ice.state = newstate
 end
 
 return Ice
--- test-ice.lua
+++ test-ice.lua
@@ -8,129 +8,129 @@ ICE = require('ice')
 TestICE = {}
 
 function TestICE:testType()
-  local ice = ICE("Probe", 2)
-  luaunit.assertEquals(ice:getType(), ICE.types["Probe"])
+  local ice = ICE:create("Probe", 2)
+  luaunit.assertEquals(ICE:getType(ice), ICE.types["Probe"])
 end
 
 function TestICE:testRating()
-  local ice = ICE("Probe", 3)
-  luaunit.assertEquals(ice:getRating(), 3)
+  local ice = ICE:create("Probe", 3)
+  luaunit.assertEquals(ICE:getRating(ice), 3)
 end
 
 function TestICE:testRatingVersusHardware()
-  local ice = ICE("Trace", 3)
+  local ice = ICE:create("Trace", 3)
   ice.analyzedLevel = 2
-  luaunit.assertEquals(ice:getRating(true), 3)
+  luaunit.assertEquals(ICE:getRating(ice, true), 3)
 end
 
 function TestICE:testRatingAnalyzed()
-  local ice = ICE("Probe", 3)
+  local ice = ICE:create("Probe", 3)
   ice.analyzedLevel = 2
-  luaunit.assertEquals(ice:getRating(), 1)
+  luaunit.assertEquals(ICE:getRating(ice), 1)
 end
 
 function TestICE:testRatingWeakend()
-  local ice = ICE("Probe", 3)
+  local ice = ICE:create("Probe", 3)
   ice.weakened = true
-  luaunit.assertEquals(ice:getRating(), -1)
+  luaunit.assertEquals(ICE:getRating(ice), -1)
 end
 
 function TestICE:testRatingAttack()
-  local ice = ICE("Attack", 2)
-  luaunit.assertEquals(ice:getAttackRating(), 2)
+  local ice = ICE:create("Attack", 2)
+  luaunit.assertEquals(ICE:getAttackRating(ice), 2)
 end
 
 function TestICE:testRatingSensor()
-  local ice = ICE("Probe", 2)
-  luaunit.assertEquals(ice:getSensorRating(), 4)
+  local ice = ICE:create("Probe", 2)
+  luaunit.assertEquals(ICE:getSensorRating(ice), 4)
 end
 
 function TestICE:testNameMax()
-  local ice = ICE("Gateway", 100) -- a ridiculous rating
-  luaunit.assertEquals(ice:getName(), "Big Bouncer")
+  local ice = ICE:create("Gateway", 100) -- a ridiculous rating
+  luaunit.assertEquals(ICE:getName(ice), "Big Bouncer")
 end
 
 function TestICE:testNameGateway()
-  local ice = ICE("Gateway", 2)
-  luaunit.assertEquals(ice:getName(), "Fence")
+  local ice = ICE:create("Gateway", 2)
+  luaunit.assertEquals(ICE:getName(ice), "Fence")
 end
 
 function TestICE:testNameProbe()
-  local ice = ICE("Probe", 2)
-  luaunit.assertEquals(ice:getName(), "Gazer")
+  local ice = ICE:create("Probe", 2)
+  luaunit.assertEquals(ICE:getName(ice), "Gazer")
 end
 
 function TestICE:testNameGuardian()
-  local ice = ICE("Guardian", 2)
-  luaunit.assertEquals(ice:getName(), "Protector")
+  local ice = ICE:create("Guardian", 2)
+  luaunit.assertEquals(ICE:getName(ice), "Protector")
 end
 
 function TestICE:testNameTapeworm()
-  local ice = ICE("Tapeworm", 2)
-  luaunit.assertEquals(ice:getName(), "Boa")
+  local ice = ICE:create("Tapeworm", 2)
+  luaunit.assertEquals(ICE:getName(ice), "Boa")
 end
 
 function TestICE:testNameAttack()
-  local ice = ICE("Attack", 2)
-  luaunit.assertEquals(ice:getName(), "Brute")
+  local ice = ICE:create("Attack", 2)
+  luaunit.assertEquals(ICE:getName(ice), "Brute")
 end
 
 function TestICE:testNameForDataBomb()
-  local ice = ICE("Tapeworm", 100, {"databomb"} )  -- a ridiculous rating
-  luaunit.assertEquals(ice:getName(), "Da Bomb")
+  local ice = ICE:create("Tapeworm", 100, {"databomb"} )  -- a ridiculous rating
+  luaunit.assertEquals(ICE:getName(ice), "Da Bomb")
 end
 
 function TestICE:testNameForHardened()
-  local ice = ICE("Attack", 2, {"hardened"} )
-  luaunit.assertEquals(ice:getName(), "Knight")
+  local ice = ICE:create("Attack", 2, {"hardened"} )
+  luaunit.assertEquals(ICE:getName(ice), "Knight")
 end
 
 function TestICE:testNameForPhasing()
-  local ice = ICE("Attack", 2, {"phasing"} )
-  luaunit.assertEquals(ice:getName(), "Bugs")
+  local ice = ICE:create("Attack", 2, {"phasing"} )
+  luaunit.assertEquals(ICE:getName(ice), "Bugs")
 end
 
 function TestICE:testNameForCrasher()
-  local ice = ICE("Attack", 2, {"crasher"} )
-  luaunit.assertEquals(ice:getName(), "Spider")
+  local ice = ICE:create("Attack", 2, {"crasher"} )
+  luaunit.assertEquals(ICE:getName(ice), "Spider")
 end
 
 function TestICE:testNameForLethal()
-  local ice = ICE("Attack", 2, {"lethal"} )
-  luaunit.assertEquals(ice:getName(), "Cowboy")
+  local ice = ICE:create("Attack", 2, {"lethal"} )
+  luaunit.assertEquals(ICE:getName(ice), "Cowboy")
 end
 
 function TestICE:testNameTrace()
-  local ice = ICE("Trace", 2)
-  luaunit.assertEquals(ice:getName(), "Hound")
+  local ice = ICE:create("Trace", 2)
+  luaunit.assertEquals(ICE:getName(ice), "Hound")
 end
 
 function TestICE:testText()
-  local ice = ICE("Guardian", 3)
-  luaunit.assertEquals(ice:getName(), "Sentry")
+  local ice = ICE:create("Guardian", 3)
+  luaunit.assertEquals(ICE:getName(ice), "Sentry")
 end
 
 function TestICE:testNotesNotAnalyzed()
-  local ice = ICE("Attack", 2, {"dumper", "fryer"} )
+  local ice = ICE:create("Attack", 2, {"dumper", "fryer"} )
   ice.analyzed = 2
-  luaunit.assertEquals(ice:getNotes(), "Attacks intruders.")
+  luaunit.assertEquals(ICE:getNotes(ice), "Attacks intruders.")
 end
 
 function TestICE:testNotesAnalyzed()
-  local ice = ICE("Attack", 2, {"dumper", "fryer"} )
+  local ice = ICE:create("Attack", 2, {"dumper", "fryer"} )
   ice.analyzedLevel = 2
-  luaunit.assertEquals(ice:getNotes(), "Attacks intruders. Can dump your deck from the matrix. Can fry one of your hardware chips.")
+  luaunit.assertEquals(ICE:getNotes(ice), "Attacks intruders. Can dump your deck from the matrix. Can fry one of your hardware chips.")
 end
 
 function TestICE:testStateValid()
-  local ice = ICE("Gateway", 1)
-  ice:setState("guarding")
+  local ice = ICE:create("Gateway", 1)
+  ICE:setState(ice, "guarding")
 end
 
 function TestICE:testStateInValid()
   local func = function()
-    local ice = ICE("Gateway", 1)
-    ice:setState("invalid selection")
+    local ice = ICE:create("Gateway", 1)
+    ICE:setState(ice, "invalid selection")
   end
   luaunit.assertError(func)
 end
