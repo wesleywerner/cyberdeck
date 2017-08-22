2635a7261611e7c6cfe523125d3d4b9b6515a49a
Wesley <wesley.werner@gmail.com>
2017-08-22 22:17:51 +0200

    Refactor the system area code into it's own module.

--- src/system.lua
+++ src/system.lua
@@ -29,48 +29,31 @@ function System:create(size, seed)
 
 end
 
-function System:generate(entity, areafunc, layoutfunc, nodeSpecification)
+function System:generate(entity, nodeSpecification)
+
+  local Area = require("systemarea")
 
   math.randomseed(entity.seed)
-  layoutfunc = layoutfunc or self.defaultLayoutFunc
-  areafunc = areafunc or self.defaultAreasFunc
   nodeSpecification = nodeSpecification or self.defaultNodesSpecificationFunc
 
   -- The higher the system level the more areas it has.
   -- The first areas are the inner areas, the last area would be
   -- the entry point into the matrix.
-  local areas = areafunc(entity)
+  local areaCount = self:getAreaCount(entity)
 
-  for areaNo=1,areas do
+  for areaNo=1, areaCount do
 
     -- get the node spec that defines which nodes should be placed
     local spec = nodeSpecification(entity, areaNo)
+    local area = Area:create(nil, areaNo, spec)
+    table.insert(entity.areas, area)
 
-    -- get the min and max number of nodes for the specification
-    local totalNodes = self:getAreaNodeCount(spec)
-
-    -- generate the area layout, based on the number of nodes to place
-    local map = self:newMap(totalNodes*2)
-    layoutfunc(map, entity, areaNo, totalNodes)
-    self:assignNodesToMap(map, spec, totalNodes)
-    table.insert(entity.areas, map)
   end
 
-end
-
-function System:newMap(size)
-  local map = {}
-  for h=1,size do
-    map[h] = {}
-    for w=1,size do
-      map[h][w] = 0
-    end
-  end
-  return map
 end
 
 -- Return the number of areas the system will have.
-function System.defaultAreasFunc(entity)
+function System:getAreaCount(entity)
   return math.max(1, math.floor(math.log(entity.size) * 1.5))
   -- by default the number of areas is logarithmic to entity size.
   -- level 1 has 1 area
@@ -80,41 +63,6 @@ function System.defaultAreasFunc(entity)
   -- for i=1,20 do
   -- print("level " .. i .. " has " .. math.floor(math.log(i)*1.5) .. " areas")
   -- end
-end
-
-function System.defaultLayoutFunc(map, systemEntity, areaNo, nodeCount)
-  local UP=1
-  local DN=2
-  local LT=3
-  local RT=4
-  local mapsize=#map[1]
-  local x,y=math.floor(mapsize/2),math.floor(mapsize/2)
-
-  print(string.format("map size is %d. middle is %d/%d", mapsize, x, y))
-  --local nodes=4+math.floor(math.log(systemEntity.size) * 2)
-
-  -- Move in a random direction until an empty map space is encountered.
-  -- this always creates the requested number of nodes, by walking over
-  -- existing nodes until a free space is encountered. Every node will
-  -- always be adjacent to at least one other node.
-  for nsteps=1,nodeCount do
-    map[x][y] = -1
-    while map[x][y] == -1 do
-      local dir = math.random(1,4)
-      if dir == UP then
-        y=math.max(1,y-1)
-      elseif dir == DN then
-        y=math.min(mapsize,y+1)
-      elseif dir == LT then
-        x=math.max(1, x-1)
-      elseif dir == RT then
-        x=math.min(mapsize,x+1)
-      end
-    end
-  end
-
-  --return map
-
 end
 
 -- Return a table defining the nodes that should appear in the system.
@@ -246,84 +194,11 @@ function System.defaultNodesSpecificationFunc(entity, areaNo)
 
 end
 
--- Get the number of nodes for an area
-function System:getAreaNodeCount(nodeDef)
-
-  -- sum the minimum and spare values for all definition entries
-  local minimum, spare = 0, 0
-
-  for k,entry in ipairs(nodeDef) do
-    minimum = minimum + entry.minimum
-    spare = spare + entry.spare
-  end
-
-  -- take the minimum plus a random number of spare
-  local total = minimum
-
-  if spare > 0 then
-    total = total + math.random(spare)
-  end
-
-  -- testing output
-  print(string.format("area can have %d to %d nodes. I decided on %d.", minimum, minimum+spare, total))
-
-  return total
-
-end
-
--- Take the map layout and assign the node specification to fill in
--- the nodes.
-function System:assignNodesToMap(map, spec, nodeCount)
-
-  local mapsize = #map[1]
-
-  local setMapValue = function(value)
-    local x,y=1,1
-    while map[x][y] == 0 or map[x][y] > 0 do
-      x = math.random(mapsize)
-      y = math.random(mapsize)
-    end
-    --print(string.format("setting map %d/%d",x,y))
-    map[x][y]=value
-  end
-
-  -- there are nodes left to assign
-  while nodeCount > 0 do
-
-    -- track whether minimum required nodes are left to place, they are placed first.
-    local hasMinimumLeft = false
-
-    -- look at the node specification for guidance
-    for entryIndex,entry in ipairs(spec) do
-
-      if nodeCount > 0 then
-
-        -- assign the minimum number of nodes required
-        if entry.minimum > 0 then
-          entry.minimum = entry.minimum - 1
-          nodeCount = nodeCount - 1
-          hasMinimumLeft = true
-          setMapValue(entryIndex)
-        end
-
-        -- place optional maximum nodes if all minimum has been placed
-        if not hasMinimumLeft and entry.spare > 0 then
-          entry.spare = entry.spare - 1
-          nodeCount = nodeCount - 1
-          setMapValue(entryIndex)
-        end
-
-      end
-    end
-  end
-end
-
 function System:print(entity)
   for i,area in ipairs(entity.areas) do
-    local mapsize=#area[1]
-    for h=1,mapsize do
-      for w=1,mapsize do
-        local value = area[w][h]
+    for h=1, area.mapsize do
+      for w=1, area.mapsize do
+        local value = area.map[w][h]
         if value == -1 then
           io.write("+")
         elseif value > 0 then
---
+++ src/systemarea.lua
@@ -0,0 +1,155 @@
+--[[
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see http://www.gnu.org/licenses/.
+]]--
+
+local Area = {}
+
+function Area:create(db, number, definition)
+
+  local instance = {}
+  instance.number = number
+  instance.definition = definition
+  instance.nodeCount = self:getnumberdeCount(db, definition)
+  -- map size is padded for extra room.
+  instance.mapsize = instance.nodeCount*2
+  instance.map = {}
+
+  -- Fill the map with blank values
+  for h=1, instance.mapsize do
+    instance.map[h] = {}
+    for w=1, instance.mapsize do
+      instance.map[h][w] = 0
+    end
+  end
+
+  self:generateLayout(db, instance)
+  self:assignNodesToMap(db, instance)
+
+  return instance
+
+end
+
+-- Get the number of nodes for an area
+function Area:getnumberdeCount(db, definition)
+
+  -- sum the minimum and spare values for all definition entries
+  local minimum, spare = 0, 0
+
+  for k,entry in ipairs(definition) do
+    minimum = minimum + entry.minimum
+    spare = spare + entry.spare
+  end
+
+  -- take the minimum plus a random number of spare
+  local total = minimum
+
+  if spare > 0 then
+    total = total + math.random(spare)
+  end
+
+  -- testing output
+  print(string.format("area can have %d to %d nodes. I decided on %d.", minimum, minimum+spare, total))
+
+  return total
+
+end
+
+-- Fill node positions on the map with a placeholder value.
+function Area:generateLayout(db, entity)
+
+  local UP=1
+  local DN=2
+  local LT=3
+  local RT=4
+  local x,y=math.floor(entity.mapsize/2),math.floor(entity.mapsize/2)
+
+  print(string.format("map size is %d. middle is %d/%d", entity.mapsize, x, y))
+
+  -- Move in a random direction until an empty map space is encountered.
+  -- this always creates the requested number of nodes, by walking over
+  -- existing nodes until a free space is encountered. Every node will
+  -- always be adjacent to at least one other node.
+  for nsteps=1, entity.nodeCount do
+
+    -- revserve this space for a node
+    entity.map[x][y] = -1
+
+    -- move alont the map until a blank space is found
+    while entity.map[x][y] == -1 do
+      local dir = math.random(1,4)
+      if dir == UP then
+        y=math.max(1,y-1)
+      elseif dir == DN then
+        y=math.min(entity.mapsize,y+1)
+      elseif dir == LT then
+        x=math.max(1, x-1)
+      elseif dir == RT then
+        x=math.min(entity.mapsize,x+1)
+      end
+    end
+  end
+
+end
+
+-- Replace placeholders on the map with items from the node definition.
+-- The minimum required nodes are placed first, and the spare nodes
+-- fill in the remaining placeholders.
+function Area:assignNodesToMap(db, entity)
+
+  -- Puts a value in a random placeholder
+  local setMapValue = function(value)
+    local x,y=1,1
+    -- scan for placeholders only (-1)
+    while entity.map[x][y] >= 0 do
+      x = math.random(entity.mapsize)
+      y = math.random(entity.mapsize)
+    end
+    entity.map[x][y] = value
+  end
+
+  local nodeCount = entity.nodeCount
+
+  -- there are nodes left to assign
+  while nodeCount > 0 do
+
+    -- track whether minimum required nodes are left to place
+    local hasMinimumLeft = false
+
+    -- look at the node specification for guidance
+    for entryIndex, entry in ipairs(entity.definition) do
+
+      -- we must test this inside the loop to avoid an infinite loop
+      if nodeCount > 0 then
+
+        -- assign the minimum number of nodes required
+        if entry.minimum > 0 then
+          entry.minimum = entry.minimum - 1
+          nodeCount = nodeCount - 1
+          hasMinimumLeft = true
+          setMapValue(entryIndex)
+        end
+
+        -- place optional maximum nodes if all minimum has been placed
+        if not hasMinimumLeft and entry.spare > 0 then
+          entry.spare = entry.spare - 1
+          nodeCount = nodeCount - 1
+          setMapValue(entryIndex)
+        end
+
+      end
+    end
+  end
+end
+
+return Area
